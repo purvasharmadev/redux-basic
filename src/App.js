135d5e98b1e2b4d183aef7b12a1326b3af822f6e
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-06-14 10:40:53 +0530

    feat/ creating a redux store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import "./styles.css";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Redux</h1>
+      <h2>State Management Library</h2>
+      <Profile />
+
+      <Login />
+
+      {/* <h3>What is state management?</h3>
+      <p>
+        <span style={highlight}>State</span> is that changeable variable which
+        updates the UI based on the changes. Ex. dark and light in a theme are
+        state which changes the UI on user Action.
+      </p>
+
+      <p>
+        sometimes states need to passed through different components. for ex. a
+        login components is responsible for user creation and a profile
+        component is responsible for showing/ edit the user information.
+        <span style={highlight}>
+          React does not allow state sharing between sibling components i.e in
+          react SPA all components are rendered inside a root div i.e APP so all
+          components acts as siblings so passing states between them is not
+          allowed One way to do that is to use props which can be useful for
+          small use cases but as the number of states increases the it gets
+          hectic pass props to every component so to manage this state passing
+          situation redux was introduced which helps in managing the state by
+          making them globally available in a <span style={keyword}>store</span>
+        </span>
+      </p>
+
+      <h2>What are we building?</h2>
+      <h3>A user login system</h3>
+      <p>
+        {" "}
+        User Story: user can login with information and can see its information
+        on page after login
+      </p>
+      <h3>depedency: redux, react-redux, @reduxjs /toolkit </h3>
+      <ul>
+        <li>Create components: Login, Profile</li>
+      </ul> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+
+// index. js is the highest level of our application
+// because this is where we are rendering our app
+
+// So to create anything global or accessable
+// to all the components we will define it here
+
+// To create a store: a global object container which holds all the states
+
+// step-1 ; configure a store using @reduxjs/toolkit
+import { configureStore } from "@reduxjs/toolkit";
+
+// step-2: provide store to app
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  // store is a collection of reducers
+  reducer: {}
+});
+
+// to provide the above store and collection of reducer througout out app we use <Provider> from react-redux and pass store as prop
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
